f005e81457d892e78cc47a8cd6d69730afa43048
isseii10 <t1l.g2.o11g@gmail.com>
2025-07-10 08:21:52 +0900

    134d

--- atcoder/abc134/src/bin/d.rs
+++ atcoder/abc134/src/bin/d.rs
@@ -5,8 +5,40 @@ use proconio::{input, marker::Chars};
 fn main() {
     input! {
         n: usize,
-        q: usize,
-        s: Chars,
-        queries: [(usize, usize); q],
+        a: [usize; n],
     }
+    let aa: Vec<usize> = std::iter::once(0).chain(a).collect();
+    let mut b = vec![0; n + 1];
+
+    for i in (1..=n).rev() {
+        let mut odd = 0; // 0 or 1
+        for j in (1..=n / i).rev() {
+            if j == 1 {
+                // println!("aa[i]: {}, odd: {}", aa[i], odd);
+                b[i] = aa[i] ^ odd;
+            } else {
+                // println!("b[i*j]: {}, odd: {}", b[i * j], odd);
+                odd = b[i * j] ^ odd;
+            }
+        }
+    }
+
+    let ans_n = b.iter().skip(1).filter(|&&x| x == 1).count();
+    println!("{}", ans_n);
+    if ans_n == 0 {
+        return;
+    }
+    let ans: Vec<usize> = b
+        .iter()
+        .enumerate()
+        .skip(1)
+        .filter_map(|(i, &x)| if x == 1 { Some(i) } else { None })
+        .collect();
+    for (i, x) in ans.iter().enumerate() {
+        if i > 0 {
+            print!(" ");
+        }
+        print!("{}", x);
+    }
+    println!();
 }
