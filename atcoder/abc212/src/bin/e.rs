bffb52ae10784ddfb5dbcd669c069e9d242aafe7
isseii10 <t1l.g2.o11g@gmail.com>
2025-07-20 16:46:56 +0900

    212e

--- atcoder/abc212/src/bin/e.rs
+++ atcoder/abc212/src/bin/e.rs
@@ -22,6 +22,8 @@ type Deque<T> = VecDeque<T>;
 #[allow(dead_code)]
 type Heap<T> = BinaryHeap<T>;
 
+type Mint = ac_library::ModInt998244353;
+
 fn main() {
     input! {
         n: usize,
@@ -29,32 +31,32 @@ fn main() {
         k: usize,
         uv: [(usize, usize); m],
     }
-    let _mod: usize = 998244353;
-    // graphは使えない辺
-    let mut graph = vec![vec![]; n];
+    // non_edgesは使えない辺
+    let mut non_edges = vec![vec![]; n];
     for i in 0..n {
-        graph[i].push(i);
+        non_edges[i].push(i);
     }
     for (u, v) in uv {
-        graph[u - 1].push(v - 1);
-        graph[v - 1].push(u - 1);
+        non_edges[u - 1].push(v - 1);
+        non_edges[v - 1].push(u - 1);
     }
 
     // dp[i][j]: i日目にjにいる場合の数
-    let mut dp = vec![vec![0; n]; k + 1];
-    dp[0][0] = 1;
-    for now in 0..k {
-        // println!("now: {}", now);
-        let mut total = 0;
+    let mut dp = vec![vec![Mint::new(0); n]; k + 1];
+    dp[0][0] = Mint::new(1);
+    for i in 0..k {
+        let mut total = Mint::new(0);
         for v in 0..n {
-            total = (total + dp[now][v]) % _mod
+            total += dp[i][v]
         }
-        // 一見O(n^2)の計算量に見えるが、実際はO(m)で済む(ならし計算量)
-        for to in 0..n {
-            dp[now + 1][to] = total;
-            for from in graph[to].iter() {
-                dp[now + 1][to] = (dp[now + 1][to] + _mod - dp[now][*from]) % _mod;
+        // 一見O(nm)の計算量に見えるが、たかだか辺の数しか処理しないので実際はO(m)で済む
+        for v in 0..n {
+            let mut to_v = total;
+            for &u in non_edges[v].iter() {
+                // uからvへはいけないので引く
+                to_v -= dp[i][u];
             }
+            dp[i + 1][v] = to_v;
         }
     }
 
