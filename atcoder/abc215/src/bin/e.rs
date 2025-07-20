789d71f4309d039fa2b383d36f4687b114425213
isseii10 <t1l.g2.o11g@gmail.com>
2025-07-21 03:52:13 +0900

    215

--- atcoder/abc215/src/bin/e.rs
+++ atcoder/abc215/src/bin/e.rs
@@ -23,56 +23,54 @@ type Deque<T> = VecDeque<T>;
 #[allow(dead_code)]
 type Heap<T> = BinaryHeap<T>;
 
+type Mint = ac_library::ModInt998244353;
+
 fn main() {
     input! {
         n: usize,
         s: Chars,
     }
-    let _mod: usize = 998244353;
     let pow2_10 = pow(2, 10);
 
     // dp[i][j][k]: i番目まで見て、j(選んだコンテストの英字をbitで管理)のコンテストに出て、選んだ最後のコンテストがkの時の場合の数
-    let mut dp = vec![vec![vec![0; 10]; pow(2, 10)]; n + 1];
-    dp[0][0][0] = 1;
+    let mut dp = vec![vec![vec![Mint::new(0); 10]; pow(2, 10)]; n + 1];
+    dp[0][0][0] = Mint::new(1);
 
-    let contests: Vec<usize> = s
-        .iter()
-        .map(|&x| x as u8 - b'A')
-        .map(|x| x as usize)
-        .collect();
+    let contests: Vec<usize> = s.iter().map(|&x| (x as u8 - b'A') as usize).collect();
 
     for i in 0..n {
-        let contest = contests[i];
-        // 初回選ぶときの遷移
-        dp[i + 1][1 << contest][contest] += (dp[i + 1][1 << contest][contest] + 1) % _mod;
+        let now_c = contests[i];
+
+        // 初出場
+        dp[i + 1][1 << now_c][now_c] += 1;
 
         for j in 0..pow2_10 {
-            for k in 0..10 {
-                if (j >> k) & 1 == 0 {
-                    // dp[i][j][k]でkが選ばれているのにjにkが含まれていない場合はありえない
+            for last_c in 0..10 {
+                if (j >> last_c) & 1 == 0 {
+                    // last_cに出場しているのにjにlast_cが含まれていない場合はありえない
                     continue;
                 }
-                // 選ばない
-                dp[i + 1][j][k] = (dp[i + 1][j][k] + dp[i][j][k]) % _mod;
+                // now_cに出場しない
+                dp[i + 1][j][last_c] = dp[i + 1][j][last_c] + dp[i][j][last_c];
 
-                // 選ぶ
-                if k == contest {
-                    // 選ぶ1: 出たことあるから最後のコンテストが同じなら選べる
-                    dp[i + 1][j][k] = (dp[i + 1][j][k] + dp[i][j][k]) % _mod;
-                } else if (j >> contest) & 1 == 0 {
-                    // 選ぶ2: 出たことないから選べる
-                    dp[i + 1][j | (1 << contest)][contest] =
-                        (dp[i + 1][j | (1 << contest)][contest] + dp[i][j][k]) % _mod
+                // now_cに出場する
+                if now_c == last_c {
+                    // 出場する1: 最後のコンテストが同じなら出場できる
+                    dp[i + 1][j][now_c] = dp[i + 1][j][now_c] + dp[i][j][last_c];
+                }
+                if now_c != last_c && (j >> now_c) & 1 == 0 {
+                    // 選ぶ2: 最後のコンテストとは異なるが、まだ出たことない種類のコンテストだから出場できる
+                    dp[i + 1][j | (1 << now_c)][now_c] =
+                        dp[i + 1][j | (1 << now_c)][now_c] + dp[i][j][last_c]
                 }
             }
         }
     }
 
-    let mut ans = 0;
-
+    let mut ans = Mint::new(0);
     for j in 0..pow2_10 {
         for k in 0..10 {
-            ans = (ans + dp[n][j][k]) % _mod
+            ans += dp[n][j][k]
         }
     }
     println!("{}", ans)
