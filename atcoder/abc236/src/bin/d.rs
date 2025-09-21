e80735443147b5656bd05f320e3ce5af6116f0f9
isseii10 <t1l.g2.o11g@gmail.com>
2025-09-21 20:38:38 +0900

    236

--- atcoder/abc236/src/bin/d.rs
+++ atcoder/abc236/src/bin/d.rs
@@ -43,18 +43,17 @@ fn main() {
     }
     // println!("{:?}", a);
     let mut ans = 0;
-    dfs(&a, n2, &mut ans, 0, 0);
+    dfs(&a, n2, &mut ans, &mut 0, 0);
     println!("{}", ans);
 }
 
-fn dfs(a: &[Vec<usize>], n2: usize, ans: &mut usize, s: usize, x: usize) {
+fn dfs(a: &[Vec<usize>], n2: usize, ans: &mut usize, s: &mut usize, x: usize) {
     let mut u = None;
-    let mut ns = s;
     for i in 0..n2 {
-        if (s >> i) & 1 == 0 {
+        if (*s >> i) & 1 == 0 {
             // まだ選んでない中で最小をuとする
             u = Some(i);
-            ns |= 1 << i;
+            *s |= 1 << i;
             break;
         }
     }
@@ -66,11 +65,12 @@ fn dfs(a: &[Vec<usize>], n2: usize, ans: &mut usize, s: usize, x: usize) {
     let u = u.unwrap();
 
     for v in 0..n2 {
-        if (ns >> v) & 1 == 0 {
+        if (*s >> v) & 1 == 0 {
             // まだ選んでない
-            ns |= 1 << v;
-            dfs(a, n2, ans, ns, x ^ a[u][v]);
-            ns &= !(1 << v);
+            *s |= 1 << v;
+            dfs(a, n2, ans, s, x ^ a[u][v]);
+            *s &= !(1 << v);
         }
     }
+    *s &= !(1 << u);
 }
